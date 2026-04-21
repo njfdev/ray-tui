8de5de1b5c313bb25327fc2ff5cf1824529b02e4
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2026-04-21 11:50:20 -0400

    fix: remove flicker from tui rerendering

--- src/main/tui.cpp
+++ src/main/tui.cpp
@@ -3,12 +3,18 @@
 #include <iostream>
 
 std::array<int, 3> calcColor(int x, int y, int width, int height) {
-    return {(int)((((double)x)/width)*255.0), 0, (int)((((double)y)/height)*255)};
+    return {(int)((((double)x)/width)*255.0), (int)((((double)y)/height)*255), 0};
 }
 
 void Tui::render() {
     int width, height;
     getTerminalDimensions(&width, &height);
+
+    // hide cursor
+    std::cout << "\033[?25l";
+
+    // move to 0,0
+    std::cout << "\033[H";
 
     // fill screen
     for (int r = 0; r < height; r++) {
